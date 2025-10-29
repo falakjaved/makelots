b2d946d2563e9303e36ce1d2e4c47f2c03ab36a4
falak <falakfalakljaved@gmail.com>
2025-10-29 14:12:41 -0700

    file

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,12 +1,12 @@
 export const portfolio = [
   {
     title: "Local Bakery Website",
-    image: "/images/portfolio.jpg",
+    image: "/images/blog.png",
     description: "Custom website for a local bakery."
   },
   {
     title: "Health Coach Landing Page",
-    image: "/images/portfolio.jpg",
+    image: "/images/logo.png",
     description: "Landing page for a wellness coach."
   }
 ];
